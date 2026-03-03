84cef0d29eabb9b10ae93bb35d948e8f5d244c2c
David Montenegro <68624139+Dmon4516@users.noreply.github.com>
2026-03-03 12:08:17 +0100

    pre Martes 3

--- Practicas_Grupo/Lexer.py
+++ Practicas_Grupo/Lexer.py
@@ -17,6 +17,11 @@ if TYPE_CHECKING:
 
 class Comentario(Lexer):
     tokens = {}
+    # TODO: Implementar contador de nesting para comentarios anidados
+    # nested = 1  # iniciar en 1 cuando se entra a Comentario
+    nested = 0  # iniciar en 0, incrementar al entrar a Comentario, decrementar al salir
+    ignore = ''
+    
     @_(r'.')
     def PASAR(self, t):
         pass
@@ -25,7 +30,10 @@ class Comentario(Lexer):
         self.lineno += 1
     @_(r'\*\)')
     def VOLVER(self, t):
-        self.begin(CoolLexer)
+        if self.nested == 1:
+            self.begin(CoolLexer)
+        else:
+            self.nested -= 1
 
 
 
@@ -39,39 +47,9 @@ class CoolLexer(Lexer):
     }
     ignore = '\t '
 
-    literals = {
-        '+', '-', '*', '/', '(', ')',
-        '<', '=', '.', ',', ';', ':',
-        '@', '{', '}', '~',
-    }
-
-    string_escapes = {
-        '\\b', '\b',
-        '\\t', '\t',
-        '\\n', '\n',
-        '\\r', '\r',
-    }
-
-    ELSE = r'\b[eE][lL][sS][eE]\b'
-
-    @_(r'\b[t][r][u][e]\b|\b[f][a][l][s][e]\b')
-    def BOOL_CONST(self, t):
-        if t.value.lower() == 'true':
-            t.value = True
-        else:            
-            t.value = False
-        return t
-    
-    @_(r'"[a-z][A-Z0-9_a-z]*"')
-    def OBJECTID(self, t):
-        palabras_reservadas = {'else', 'if', 'fi', 'then', 'not', 'in', 'case', 'esac', 'class', 'inherits', 'isvoid', 'let', 'loop', 'new', 'of', 'pool', 'while', 'true', 'false'}
-        if t.value.lower() in palabras_reservadas:
-            t.type = t.value.upper()
-        return t
-    
-    @_(r'\b\d+\b')
-    def INT_CONST(self, t):
-        return t
+    @_(r'--.*')
+    def COMMENT(self, t):
+        self.lineno += 1
     
     @_(r'[<][=]')
     def LE(self, t):
@@ -85,11 +63,46 @@ class CoolLexer(Lexer):
     def DARROW(self, t):
         return t
     
+    @_(r'\b[t][r][u][e]\b|\b[f][a][l][s][e]\b')
+    def BOOL_CONST(self, t):
+        if t.value == 'true':
+            t.value = True
+        elif t.value == 'false':            
+            t.value = False
+        return t
+
+    literals = {
+        '+', '-', '*', '/', '(', ')',
+        '<', '=', '.', ',', ';', ':',
+        '@', '{', '}', '>', '~', 
+    }
+
+    string_escapes = {
+        '\\b', '\b',
+        '\\t', '\t',
+        '\\n', '\n',
+        '\\r', '\r',
+    }
+
+    
+    @_(r'[a-z][A-Z0-9_a-z]*')
+    def OBJECTID(self, t):
+        palabras_reservadas = {'else', 'if', 'fi', 'then', 'not', 'in', 'case', 'esac', 'class', 'inherits', 'isvoid', 'let', 'loop', 'new', 'of', 'pool', 'while', 'true', 'false'}
+        if t.value.lower() in palabras_reservadas:
+            t.type = t.value.upper()
+            # TODO: Verificar si verdadero es solo si la forma es 'true' o 'false' en minúscula
+            # Si es 'TRUE' o 'FALSE' debe ser TYPEID, no BOOL_CONST
+        return t
+    
+    @_(r'\b\d+\b')
+    def INT_CONST(self, t):
+        return t
+    
     @_(r'\s+')
     def SPACE(self, t):
         return t
     
-    @_(r'\w+')
+    @_(r'\b[A-Z][A-Za-z0-9_]*\b')
     def TYPEID(self, t):
         return t
     
@@ -97,30 +110,37 @@ class CoolLexer(Lexer):
     def LINEBREAK(self, t):
         self.lineno += 1
     
-    @_(r'\b[wW][hH][iI][lL][eE]\b')
-    def WHILE(self, t):
-        return t
-    
-
     @_(r'"([^"\\]|\\.)*"')
     def STR_CONST(self, t):
+        # TODO: Procesar escapes: \\b -> \b, \\t -> \t, \\n -> \n, \\r -> \r
+        # TODO: Validar longitud máxima 1024 caracteres
+        # TODO: Rechazar si contiene: \0, EOF, o saltos de línea sin escape
+        # TODO: Quitar las comillas del valor final
         if t.value == self.string_escapes:
             return t
     
     
 
     def error(self, t):
-        self.index += 1
+        t.type = 'ERROR'
+        t.value = t.value[0]
+        self.lineno += 1
+        return t
 
     CARACTERES_CONTROL = [bytes.fromhex(i+hex(j)[-1]).decode('ascii')
                           for i in ['0', '1']
                           for j in range(16)] + [bytes.fromhex(hex(127)[-2:]).decode("ascii")]
 
+    # TODO: Implementar patrón para comentarios anidados sin contar los escapados
+    # @_(r'\(\*[^\\]\)')
     @_(r'\(\*')
     def IR(self, t):
+        # Iniciar estado de comentario anidado
         self.begin(Comentario)
         
     def salida(self, texto):
+        # TODO: Revisar formato de salida con los archivos de ejemplo
+        # TODO: Verificar que STR_CONST y otros tokens tengan el formato correcto
         lexer = CoolLexer()
         list_strings = []
         for token in lexer.tokenize(texto):
