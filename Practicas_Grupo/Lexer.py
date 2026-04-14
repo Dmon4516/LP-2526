e9945a9a7561f3bbeb52283f7d5fd5c36c79a189
Christian <cff638@alumnos.unican.es>
2026-04-14 15:23:09 +0200

    Nuevas versiones de Lexer y Parser

--- Practicas_Grupo/Lexer.py
+++ Practicas_Grupo/Lexer.py
@@ -15,159 +15,274 @@ if TYPE_CHECKING:
             return func
         return decorate
 
-class Comentario(Lexer):
-    tokens = {}
-    nested = 1
-    @_(r'\(\*')
-    def ABRIR(self, t):
-        self.nested += 1
-
-    @_(r'\*\)')
-    def VOLVER(self, t):
-        self.nested -= 1
-        if self.nested == 0:
-            self.begin(CoolLexer)
-
-    @_(r'\n')
-    def LINEA(self, t):
-        self.lineno += 1
-
-    @_(r'.')
-    def PASAR(self, t):
-        pass
-
 
 class CoolLexer(Lexer):
     tokens = {
         'OBJECTID', 'INT_CONST', 'BOOL_CONST', 'TYPEID',
         'ELSE', 'IF', 'FI', 'THEN', 'NOT', 'IN', 'CASE', 'ESAC', 'CLASS',
         'INHERITS', 'ISVOID', 'LET', 'LOOP', 'NEW', 'OF',
-        'POOL', 'WHILE', 'STR_CONST', 'LE', 'DARROW', 'ASSIGN', 'SPACE'
-    }
-    ignore = '\t '
-
-    palabras_reservadas = {
-        'else', 'if', 'fi', 'then', 'not', 'in', 'case', 'esac', 'class',
-        'inherits', 'isvoid', 'let', 'loop', 'new', 'of', 'pool', 'while',
-        'true', 'false'
+        'POOL', 'WHILE', 'STR_CONST', 'LE', 'DARROW', 'ASSIGN'
     }
 
-    @_(r'--.*')
-    def COMMENT(self, t):
-        self.lineno += 1
-    
-    @_(r'[<][=]')
-    def LE(self, t):
-        return t
-    
-    @_(r'[<][-]')
-    def ASSIGN(self, t):
-        return t
-    
-    @_(r'[=][>]')
-    def DARROW(self, t):
-        return t
-    
-    @_(r'[t][r][u][e]|[f][a][l][s][e]')
-    def BOOL_CONST(self, t):
-        if t.value == 'true':
-            t.value = True
-        elif t.value == 'false':            
-            t.value = False
-        return t
+    ignore = ' \t\x0b\x0c'
 
     literals = {
-        '+', '-', '*', '/', '(', ')',
-        '<', '=', '.', ',', ';', ':',
-        '@', '{', '}', '>', '~', 
+        '+', '-', '*', '/', '(', ')', '<',
+        '.', ',', ';', ':', '@', '{', '}', '~', '='
     }
 
-    string_escapes = {
-        '\\b': '\b',
-        '\\t': '\t',
-        '\\n': '\n',
-        '\\r': '\r',
-    }
+    LE     = r'<='
+    DARROW = r'=>'
+    ASSIGN = r'<-'
 
-    
-    @_(r'[a-z][A-Z0-9_a-z]*')
-    def OBJECTID(self, t):
-        if t.value.lower() in self.palabras_reservadas:
-            t.type = t.value.upper()
-        return t
-    
-    @_(r'\b\d+\b')
+    @_(r'[0-9]+')
     def INT_CONST(self, t):
+        t.value = str(t.value)
         return t
-    
-    @_(r'\b[A-Z][A-Za-z0-9_]*\b')
-    def TYPEID(self, t):
-        return t
-    
-    @_(r'\n')
-    def LINEBREAK(self, t):
-        self.lineno += 1
-    
-    @_(r'"([^"\\]|\\.)*"')
+
+    @_(r'"')
     def STR_CONST(self, t):
+        string_value = ''
+        error_msg = None
+        i = self.index
+        text = self.text
+        length = len(text)
 
-        valor = t.value[1:-1]  # quitar las comillas
-        
-        if '\n' in valor or '\0' in valor:
+        while i < length:
+            c = text[i]
+
+            if c == '\x00':
+                error_msg = 'String contains null character.'
+                i += 1
+                while i < length and text[i] != '"' and text[i] != '\n':
+                    i += 1
+                if i < length and text[i] == '"':
+                    i += 1
+                break
+
+            elif c == '\\':
+                i += 1
+                if i >= length:
+                    error_msg = 'EOF in string constant'
+                    break
+                nc = text[i]
+                if nc == '\x00':
+                    error_msg = 'String contains escaped null character.'
+                    i += 1
+                    while i < length and text[i] != '"' and text[i] != '\n':
+                        i += 1
+                    if i < length and text[i] == '"':
+                        i += 1
+                    break
+                elif nc == 'n':
+                    string_value += '\n'
+                elif nc == 't':
+                    string_value += '\t'
+                elif nc == 'b':
+                    string_value += '\b'
+                elif nc == 'f':
+                    string_value += '\f'
+                elif nc == '\\':
+                    string_value += '\\'
+                elif nc == '"':
+                    string_value += '"'
+                elif nc == '\n':
+                    self.lineno += 1
+                    string_value += '\n'
+                else:
+                    string_value += nc
+                i += 1
+
+            elif c == '"':
+                i += 1
+                break
+
+            elif c == '\n':
+                error_msg = 'Unterminated string constant'
+                self.lineno += 1
+                i += 1
+                break
+
+            else:
+                string_value += c
+                i += 1
+
+        else:
+            if error_msg is None:
+                error_msg = 'EOF in string constant'
+
+        self.index = i
+
+        if error_msg:
             t.type = 'ERROR'
-            t.value = 'Invalid string constant'
+            t.value = error_msg
             return t
-        
-        # Reemplazar escapes
-        for escape, char in self.string_escapes.items():
-            valor = valor.replace(escape, char)
 
-        if len(valor) > 1024:
+        if len(string_value) > 1024:
             t.type = 'ERROR'
             t.value = 'String constant too long'
-        else:
-            t.value = valor
+            return t
 
+        t.value = string_value
         return t
+
+    @_(r'--[^\n]*')
+    def COMENTARIO_LINEA(self, t):
+        pass
+
+    @_(r'\(\*')
+    def COMENTARIO_MULTI(self, t):
+        nivel = 1
+        i = self.index
+        text = self.text
+        length = len(text)
+
+        while i < length and nivel > 0:
+            c = text[i]
+            if c == '"':
+                i += 1
+                while i < length:
+                    cc = text[i]
+                    if cc == '\\':
+                        i += 2 
+                    elif cc == '"':
+                        i += 1
+                        break
+                    elif cc == '\n':
+                        self.lineno += 1
+                        i += 1
+                    else:
+                        i += 1
+            elif c == '(' and i + 1 < length and text[i + 1] == '*':
+                nivel += 1
+                i += 2
+            elif c == '*' and i + 1 < length and text[i + 1] == ')':
+                nivel -= 1
+                i += 2
+            elif c == '\n':
+                self.lineno += 1
+                i += 1
+            else:
+                i += 1
+
+        if nivel > 0:
+            t.type = 'ERROR'
+            t.value = 'EOF in comment'
+            self.index = i
+            return t
+
+        self.index = i
+
+    @_(r'\*\)')
+    def COMENTARIO_CIERRE(self, t):
+        t.type = 'ERROR'
+        t.value = 'Unmatched *)'
+        return t
+
+    @_(r'\n')
+    def NEWLINE(self, t):
+        self.lineno += 1
+
+    @_(r'\r')
+    def CR(self, t):
+        pass
+
+    @_(r'[a-zA-Z][a-zA-Z0-9_]*')
+    def IDENTIFIER(self, t):
+        val_lower = t.value.lower()
+
+        if t.value[0].islower() and val_lower == 'true':
+            t.type = 'BOOL_CONST'
+            t.value = True
+            return t
+        if t.value[0].islower() and val_lower == 'false':
+            t.type = 'BOOL_CONST'
+            t.value = False
+            return t
+
+        keywords = {
+            'class': 'CLASS', 'else': 'ELSE', 'fi': 'FI',
+            'if': 'IF', 'in': 'IN', 'inherits': 'INHERITS',
+            'isvoid': 'ISVOID', 'let': 'LET', 'loop': 'LOOP',
+            'pool': 'POOL', 'then': 'THEN', 'while': 'WHILE',
+            'case': 'CASE', 'esac': 'ESAC', 'new': 'NEW',
+            'of': 'OF', 'not': 'NOT',
+        }
+        if val_lower in keywords:
+            t.type = keywords[val_lower]
+            return t
+
+        if t.value[0].isupper():
+            t.type = 'TYPEID'
+            return t
+
+        t.type = 'OBJECTID'
+        return t
+
+    CARACTERES_CONTROL = [
+        bytes.fromhex(i + hex(j)[-1]).decode('ascii')
+        for i in ['0', '1']
+        for j in range(16)
+    ] + [bytes.fromhex(hex(127)[-2:]).decode("ascii")]
 
     def error(self, t):
+        char = t.value[0]
+        code = ord(char)
+        if code < 32 or code == 127:
+            error_val = f'\\{code:03o}'
+        elif char == '\\':
+            error_val = '\\\\'
+        else:
+            error_val = char
         t.type = 'ERROR'
-        t.value = t.value[0]
-        self.lineno += 1
+        t.value = error_val
+        self.index += 1
         return t
 
-    CARACTERES_CONTROL = [bytes.fromhex(i+hex(j)[-1]).decode('ascii')
-                          for i in ['0', '1']
-                          for j in range(16)] + [bytes.fromhex(hex(127)[-2:]).decode("ascii")]
+    def _str_para_salida(self, s):
+        result = ''
+        for c in s:
+            code = ord(c)
+            if c == '\n':
+                result += '\\n'
+            elif c == '\t':
+                result += '\\t'
+            elif c == '\b':
+                result += '\\b'
+            elif c == '\f':
+                result += '\\f'
+            elif c == '\\':
+                result += '\\\\'
+            elif c == '"':
+                result += '\\"'
+            elif code < 32 or code == 127:
+                result += f'\\{code:03o}'
+            else:
+                result += c
+        return result
 
-    # TODO: Implementar patrón para comentarios anidados sin contar los escapados
-    # @_(r'\(\*[^\\]\)')
-    @_(r'\(\*')
-    def IR(self, t):
-        self.begin(Comentario)
-        
     def salida(self, texto):
-        # TODO: Revisar formato de salida con los archivos de ejemplo
-        # TODO: Verificar que STR_CONST y otros tokens tengan el formato correcto
         lexer = CoolLexer()
         list_strings = []
         for token in lexer.tokenize(texto):
-            result = f'#{token.lineno} {token.type} '
-            if token.type == 'BOOL_CONST':
-                result += "true" if token.value else "false"
-            elif token.type == 'OBJECTID':
-                result += f"{token.value}"
-            elif token.type == 'TYPEID':
-                result += f"{str(token.value)}"
-            elif token.type in self.literals:
-                result = f'#{token.lineno} \'{token.type}\''
-            elif token.type == 'STR_CONST':
-                result += token.value
-            elif token.type == 'INT_CONST':
-                result += str(token.value)
-            elif token.type == 'ERROR':
+            if token.type == 'OBJECTID':
                 result = f'#{token.lineno} {token.type} {token.value}'
+            elif token.type == 'BOOL_CONST':
+                val = 'true' if token.value else 'false'
+                result = f'#{token.lineno} {token.type} {val}'
+            elif token.type == 'TYPEID':
+                result = f'#{token.lineno} {token.type} {token.value}'
+            elif token.type in self.literals:
+                result = f'#{token.lineno} \'{token.type}\' '
+            elif token.type == 'STR_CONST':
+                escaped = self._str_para_salida(token.value)
+                result = f'#{token.lineno} {token.type} "{escaped}"'
+            elif token.type == 'INT_CONST':
+                result = f'#{token.lineno} {token.type} {token.value}'
+            elif token.type == 'ERROR':
+                result = f'#{token.lineno} {token.type} "{token.value}"'
             else:
                 result = f'#{token.lineno} {token.type}'
             list_strings.append(result)
         return list_strings
+
--- Practicas_Grupo/Parser.py
+++ Practicas_Grupo/Parser.py
@@ -6,6 +6,42 @@ import sys
 import os
 from Clases import *
 
+class _RamaCase(RamaCase):
+    def str(self, n):
+        resultado = super(RamaCase, self).str(n)   
+        resultado += f'{n*" "}_branch\n'
+        resultado += f'{(n+2)*" "}{self.nombre_variable}\n'
+        resultado += f'{(n+2)*" "}{self.tipo}\n'
+        resultado += self.cuerpo.str(n+2)
+        return resultado  
+
+
+def _escape_str(s):
+    """Escapa los caracteres especiales del valor de un string para el AST."""
+    result = ''
+    for c in s:
+        code = ord(c)
+        if c == '\n':
+            result += '\\n'
+        elif c == '\t':
+            result += '\\t'
+        elif c == '\b':
+            result += '\\b'
+        elif c == '\f':
+            result += '\\f'
+        elif c == '\\':
+            result += '\\\\'
+        elif c == '"':
+            result += '\\"'
+        elif code < 32 or code == 127:
+            result += f'\\{code:03o}'
+        else:
+            result += c
+    return result
+
+
+_TOKENS_CON_VALOR = {'OBJECTID', 'TYPEID', 'INT_CONST', 'STR_CONST', 'BOOL_CONST'}
+
 
 class CoolParser(Parser):
     nombre_fichero = ''
@@ -13,25 +49,371 @@ class CoolParser(Parser):
     debugfile = "salida.out"
     errores = []
 
+    precedence = (
+        ('right', 'ASSIGN'),
+        ('left',  'NOT'),
+        ('nonassoc', '<', 'LE', '='),
+        ('left',  '+', '-'),
+        ('left',  '*', '/'),
+        ('left',  'ISVOID'),
+        ('left',  '~'),
+        ('left',  '@'),
+        ('left',  '.'),
+    )
+
+    @_("clases")
+    def Programa(self, p):
+        return Programa(secuencia=p.clases)
+
     @_("Clase ';'")
-    def Programa(self, p):
-        pass
+    def clases(self, p):
+        return [p.Clase]
 
-    
-    @_("Programa Clase ';'")
-    def Programa(self, p):
-        pass
-    
-    @_("CLASS TYPEID hereda '{'serie_atr_met '}'") 
+    @_("clases Clase ';'")
+    def clases(self, p):
+        return p.clases + [p.Clase]
+    @_("error ';'")
+    def clases(self, p):
+        return []
+
+    @_("clases error ';'")
+    def clases(self, p):
+        return p.clases
+
+    @_("clases error")
+    def clases(self, p):
+        if self.errores:
+            self.errores.pop()
+        return p.clases
+
+    @_("CLASS TYPEID '{' caracteristicas '}'")
     def Clase(self, p):
-        pass
+        return Clase(
+            linea=p.lineno,
+            nombre=p.TYPEID,
+            padre='Object',
+            nombre_fichero=self.nombre_fichero,
+            caracteristicas=p.caracteristicas
+        )
 
-    @_("", "INHERITS TYPEID")
-    def hereda(self, p):
-        pass
+    @_("CLASS TYPEID INHERITS TYPEID '{' caracteristicas '}'")
+    def Clase(self, p):
+        return Clase(
+            linea=p.lineno,
+            nombre=p.TYPEID0,
+            padre=p.TYPEID1,
+            nombre_fichero=self.nombre_fichero,
+            caracteristicas=p.caracteristicas
+        )
 
-    @_("", "atributo", "metodo", "serie_atr_met atributo", "serie_atr_met metodo")
-    def serie_atr_met(self, p):
-        pass
+    @_("")
+    def caracteristicas(self, p):
+        return []
 
+    @_("caracteristicas atributo")
+    def caracteristicas(self, p):
+        return p.caracteristicas + [p.atributo]
+
+    @_("caracteristicas metodo")
+    def caracteristicas(self, p):
+        return p.caracteristicas + [p.metodo]
+
+
+    @_("OBJECTID ':' TYPEID ';'")
+    def atributo(self, p):
+        return Atributo(
+            linea=p.lineno,
+            nombre=p.OBJECTID,
+            tipo=p.TYPEID,
+            cuerpo=NoExpr()
+        )
+
+    @_("OBJECTID ':' TYPEID ASSIGN Expresion ';'")
+    def atributo(self, p):
+        return Atributo(
+            linea=p.lineno,
+            nombre=p.OBJECTID,
+            tipo=p.TYPEID,
+            cuerpo=p.Expresion
+        )
+
+    @_("OBJECTID '(' ')' ':' TYPEID '{' Expresion '}' ';'")
+    def metodo(self, p):
+        return Metodo(
+            linea=p.lineno,
+            nombre=p.OBJECTID,
+            tipo=p.TYPEID,
+            formales=[],
+            cuerpo=p.Expresion
+        )
+
+    @_("OBJECTID '(' lista_formales ')' ':' TYPEID '{' Expresion '}' ';'")
+    def metodo(self, p):
+        return Metodo(
+            linea=p.lineno,
+            nombre=p.OBJECTID,
+            tipo=p.TYPEID,
+            formales=p.lista_formales,
+            cuerpo=p.Expresion
+        )
+
+
+    @_("Formal")
+    def lista_formales(self, p):
+        return [p.Formal]
+
+    @_("lista_formales ',' Formal")
+    def lista_formales(self, p):
+        return p.lista_formales + [p.Formal]
+
+    @_("OBJECTID ':' TYPEID")
+    def Formal(self, p):
+        return Formal(
+            linea=p.lineno,
+            nombre_variable=p.OBJECTID,
+            tipo=p.TYPEID
+        )
+
+    @_("OBJECTID ASSIGN Expresion")
+    def Expresion(self, p):
+        return Asignacion(
+            linea=p.lineno,
+            nombre=p.OBJECTID,
+            cuerpo=p.Expresion
+        )
+
+    @_("Expresion '+' Expresion")
+    def Expresion(self, p):
+        return Suma(linea=p.lineno, izquierda=p.Expresion0, derecha=p.Expresion1)
+
+    @_("Expresion '-' Expresion")
+    def Expresion(self, p):
+        return Resta(linea=p.lineno, izquierda=p.Expresion0, derecha=p.Expresion1)
+
+    @_("Expresion '*' Expresion")
+    def Expresion(self, p):
+        return Multiplicacion(linea=p.lineno, izquierda=p.Expresion0, derecha=p.Expresion1)
+
+    @_("Expresion '/' Expresion")
+    def Expresion(self, p):
+        return Division(linea=p.lineno, izquierda=p.Expresion0, derecha=p.Expresion1)
+
+    @_("Expresion '<' Expresion")
+    def Expresion(self, p):
+        return Menor(linea=p.lineno, izquierda=p.Expresion0, derecha=p.Expresion1)
+
+    @_("Expresion LE Expresion")
+    def Expresion(self, p):
+        return LeIgual(linea=p.lineno, izquierda=p.Expresion0, derecha=p.Expresion1)
+
+    @_("Expresion '=' Expresion")
+    def Expresion(self, p):
+        return Igual(linea=p.lineno, izquierda=p.Expresion0, derecha=p.Expresion1)
+
+    @_("NOT Expresion")
+    def Expresion(self, p):
+        return Not(linea=p.lineno, expr=p.Expresion)
+
+    @_("ISVOID Expresion")
+    def Expresion(self, p):
+        return EsNulo(linea=p.lineno, expr=p.Expresion)
+
+    @_("'~' Expresion")
+    def Expresion(self, p):
+        return Neg(linea=p.lineno, expr=p.Expresion)
+
+    @_("'(' Expresion ')'")
+    def Expresion(self, p):
+        return p.Expresion
+
+    @_("Expresion '@' TYPEID '.' OBJECTID '(' ')'")
+    def Expresion(self, p):
+        return LlamadaMetodoEstatico(
+            linea=p.lineno,
+            cuerpo=p.Expresion,
+            clase=p.TYPEID,
+            nombre_metodo=p.OBJECTID,
+            argumentos=[]
+        )
+
+    @_("Expresion '@' TYPEID '.' OBJECTID '(' lista_args ')'")
+    def Expresion(self, p):
+        return LlamadaMetodoEstatico(
+            linea=p.lineno,
+            cuerpo=p.Expresion,
+            clase=p.TYPEID,
+            nombre_metodo=p.OBJECTID,
+            argumentos=p.lista_args
+        )
+
+    @_("Expresion '.' OBJECTID '(' ')'")
+    def Expresion(self, p):
+        return LlamadaMetodo(
+            linea=p.lineno,
+            cuerpo=p.Expresion,
+            nombre_metodo=p.OBJECTID,
+            argumentos=[]
+        )
+
+    @_("Expresion '.' OBJECTID '(' lista_args ')'")
+    def Expresion(self, p):
+        return LlamadaMetodo(
+            linea=p.lineno,
+            cuerpo=p.Expresion,
+            nombre_metodo=p.OBJECTID,
+            argumentos=p.lista_args
+        )
+
+    @_("OBJECTID '(' ')'")
+    def Expresion(self, p):
+        return LlamadaMetodo(
+            linea=p.lineno,
+            cuerpo=Objeto(linea=p.lineno, nombre='self'),
+            nombre_metodo=p.OBJECTID,
+            argumentos=[]
+        )
+
+    @_("OBJECTID '(' lista_args ')'")
+    def Expresion(self, p):
+        return LlamadaMetodo(
+            linea=p.lineno,
+            cuerpo=Objeto(linea=p.lineno, nombre='self'),
+            nombre_metodo=p.OBJECTID,
+            argumentos=p.lista_args
+        )
+
+    @_("IF Expresion THEN Expresion ELSE Expresion FI")
+    def Expresion(self, p):
+        return Condicional(
+            linea=p.lineno,
+            condicion=p.Expresion0,
+            verdadero=p.Expresion1,
+            falso=p.Expresion2
+        )
+
+    @_("WHILE Expresion LOOP Expresion POOL")
+    def Expresion(self, p):
+        return Bucle(
+            linea=p.lineno,
+            condicion=p.Expresion0,
+            cuerpo=p.Expresion1
+        )
+
+    @_("LET lista_let IN Expresion")
+    def Expresion(self, p):
+        expr = p.Expresion
+        for nombre, tipo, init, linea in reversed(p.lista_let):
+            expr = Let(
+                linea=linea,
+                nombre=nombre,
+                tipo=tipo,
+                inicializacion=init,
+                cuerpo=expr
+            )
+        return expr
+
+    @_("binding_let")
+    def lista_let(self, p):
+        return [p.binding_let]
+
+    @_("lista_let ',' binding_let")
+    def lista_let(self, p):
+        return p.lista_let + [p.binding_let]
+
+    @_("OBJECTID ':' TYPEID")
+    def binding_let(self, p):
+        return (p.OBJECTID, p.TYPEID, NoExpr(), p.lineno)
+
+    @_("OBJECTID ':' TYPEID ASSIGN Expresion")
+    def binding_let(self, p):
+        return (p.OBJECTID, p.TYPEID, p.Expresion, p.lineno)
+
+    @_("CASE Expresion OF lista_ramas ESAC")
+    def Expresion(self, p):
+        s = Swicht(
+            linea=p.lineno,
+            expr=p.Expresion,
+            casos=p.lista_ramas
+        )
+        s.cast = '_no_type'
+        return s
+
+    @_("rama_case")
+    def lista_ramas(self, p):
+        return [p.rama_case]
+
+    @_("lista_ramas rama_case")
+    def lista_ramas(self, p):
+        return p.lista_ramas + [p.rama_case]
+
+    @_("OBJECTID ':' TYPEID DARROW Expresion ';'")
+    def rama_case(self, p):
+        return _RamaCase(
+            linea=p.lineno,
+            nombre_variable=p.OBJECTID,
+            tipo=p.TYPEID,
+            cuerpo=p.Expresion
+        )
+
+    @_("NEW TYPEID")
+    def Expresion(self, p):
+        n = Nueva(linea=p.lineno, tipo=p.TYPEID)
+        n.cast = '_no_type'
+        return n
     
+
+    @_("'{' lista_exprs '}'")
+    def Expresion(self, p):
+        return Bloque(linea=p.lineno, expresiones=p.lista_exprs)
+
+    @_("Expresion ';'")
+    def lista_exprs(self, p):
+        return [p.Expresion]
+
+    @_("lista_exprs Expresion ';'")
+    def lista_exprs(self, p):
+        return p.lista_exprs + [p.Expresion]
+
+    @_("OBJECTID")
+    def Expresion(self, p):
+        return Objeto(linea=p.lineno, nombre=p.OBJECTID)
+
+    @_("INT_CONST")
+    def Expresion(self, p):
+        return Entero(linea=p.lineno, valor=p.INT_CONST)
+
+    @_("STR_CONST")
+    def Expresion(self, p):
+        return String(linea=p.lineno, valor=f'"{_escape_str(p.STR_CONST)}"')
+
+    @_("BOOL_CONST")
+    def Expresion(self, p):
+        return Booleano(linea=p.lineno, valor=p.BOOL_CONST)
+
+
+    @_("Expresion")
+    def lista_args(self, p):
+        return [p.Expresion]
+
+    @_("lista_args ',' Expresion")
+    def lista_args(self, p):
+        return p.lista_args + [p.Expresion]
+
+
+    def error(self, p):
+        if p:
+            tok_type = p.type
+            if len(tok_type) == 1:
+                parte = f"'{tok_type}'"
+            elif tok_type in _TOKENS_CON_VALOR:
+                parte = f'{tok_type} = {p.value}'
+            else:
+                parte = tok_type
+            msg = (f'"{self.nombre_fichero}", line {p.lineno}: '
+                   f'syntax error at or near {parte}')
+            self.errores.append(msg)
+        else:
+            msg = (f'"{self.nombre_fichero}", line 0: '
+                   f'syntax error at or near EOF')
+            self.errores.append(msg)
+
